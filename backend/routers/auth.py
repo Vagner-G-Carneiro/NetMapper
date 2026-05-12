c150762111497166accada1a021a4451ccc085e8
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2026-05-12 19:47:56 -0300

    feat():Adiciona endpoint de produção e de testes via Swagger!

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from database import get_db
@@ -13,6 +14,12 @@ def register(data: UserCreate, db: Session = Depends(get_db)):
     return auth_service.register_user(db, data)
 
 
-@router.post("/login", response_model=Token)
+@router.post("/login/api", response_model=Token)
 def login(data: UserLogin, db: Session = Depends(get_db)):
+    return auth_service.login_user(db, data)
+
+#Recebe HTML form data, útil para testes no Swagger UI
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    data = UserLogin(email=form_data.username, password=form_data.password)
     return auth_service.login_user(db, data)
